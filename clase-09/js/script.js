5c764305e7fe412d582e43770bd8b0aa546859fb
Jean Paul Ferreira <jepafe@gmail.com>
2024-10-31 12:34:13 -0300

    Clase 09 - JS

---
+++ clase-09/js/script.js
@@ -0,0 +1,105 @@
+//   alert("Hola JS");
+
+// Utilizamos el console.log
+
+/* 
+console.log("Hola JS", 4 * 3);
+console.log("La suma es: ", 2 + 1); 
+*/
+
+// Variables
+
+// let num1;
+
+var num1;
+num1 = 3;
+
+let num2 = 5;
+
+let nombreCompleto;
+let nombre_completo;
+
+let suma = 3 + 5 + 7;
+
+console.log("Suma:", suma);
+
+console.log("Paul's");
+
+console.log('Un "mensaje" en la consola');
+
+// Contantes
+
+const IVA = 21;
+
+console.log("IVA:", IVA);
+
+// IVA = 10;
+
+const API_KEY = "fhfhiuqfhiuewh39r23y2";
+
+// Tipo de datos
+
+num1 = 3;
+let num3 = new Number(34);
+let num4 = Number("54");
+
+let num5 = Number("5y4");
+
+// parse...
+
+num1 = parseInt("123.5");
+num2 = parseFloat("123.51");
+
+num3 = parseInt("d123.5");
+
+// Operadores
+
+num1 = 3;
+
+suma = 1 + 4;
+let resta = 6 - 9;
+let multi = 5 * 6;
+let division = 5 / 4;
+
+let expo = 2 ** 3;
+
+num1 = 12 % 3;
+
+// Incremento y decremento
+
+// let num6 = 0;
+
+// num6 = num6 + 1;
+// console.log(num6);
+
+// num6 = num6 + 1;
+// console.log(num6);
+
+// num6 += 1;
+// console.log(num6);
+
+// num6++;
+// console.log(num6); // 4
+
+// console.log(num6++);
+// console.log(num6);
+// console.log(++num6);
+
+// Concatenación
+
+let msg1 = "Hola";
+let msg2 = "Mundo";
+
+// let msg3 = msg1 + ' ' + msg2;
+let msg3 = `El mensaje final es: ${msg1} ${msg2}`;
+
+// console.log(msg3);
+
+// Prompt
+
+// let numero = prompt("Ingrese un numero: ");
+// numero = parseInt(numero)
+// suma = 4 + numero;
+
+// const nombre = prompt("Ingrese su nombre");
+// document.write("<p>Hola " + nombre + "</p>");
